254b23a24bdb7b0d039f3760d9627facac0a8d1e
Bernardo Gurgel <brnrdog@hey.com>
2020-11-14 11:24:37 +0100

    refactor: move color functions to Color module
    Using `include` rather than just assigning the `Color` module was
    preferred in order to have a unique entry point for the bindings.

--- __tests__/PolishedTest.bs.js
+++ __tests__/PolishedTest.bs.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var Jest = require("@glennsl/bs-jest/src/jest.bs.js");
+var Curry = require("bs-platform/lib/js/curry.js");
 var Polished = require("../src/Polished.bs.js");
 
 function keepGoing(param) {
@@ -9,55 +10,55 @@ function keepGoing(param) {
 }
 
 Jest.test("shade", (function (param) {
-        return Jest.Expect.toBe("#b13c3c", Jest.Expect.expect(Polished.shade("#ed5051", 0.25)));
+        return Jest.Expect.toBe("#b13c3c", Jest.Expect.expect(Curry._2(Polished.Color.shade, "#ed5051", 0.25)));
       }));
 
 Jest.test("tint", (function (param) {
-        return Jest.Expect.toBe("#f17b7c", Jest.Expect.expect(Polished.tint("#ed5051", 0.25)));
+        return Jest.Expect.toBe("#f17b7c", Jest.Expect.expect(Curry._2(Polished.Color.tint, "#ed5051", 0.25)));
       }));
 
 Jest.test("lighten", (function (param) {
-        return Jest.Expect.toBe("#f9c4c4", Jest.Expect.expect(Polished.lighten("#ed5051", 0.25)));
+        return Jest.Expect.toBe("#f9c4c4", Jest.Expect.expect(Curry._2(Polished.Color.lighten, "#ed5051", 0.25)));
       }));
 
 Jest.test("darken", (function (param) {
-        return Jest.Expect.toBe("#ac1213", Jest.Expect.expect(Polished.darken("#ed5051", 0.25)));
+        return Jest.Expect.toBe("#ac1213", Jest.Expect.expect(Curry._2(Polished.Color.darken, "#ed5051", 0.25)));
       }));
 
 Jest.test("hsl", (function (param) {
-        return Jest.Expect.toBe("#734d26", Jest.Expect.expect(Polished.hsl(30, 0.5, 0.3)));
+        return Jest.Expect.toBe("#734d26", Jest.Expect.expect(Curry._3(Polished.Color.hsl, 30, 0.5, 0.3)));
       }));
 
 Jest.test("adjustHue", (function (param) {
-        return Jest.Expect.toBe("#ed9f50", Jest.Expect.expect(Polished.adjustHue("#ed5051", 30.5)));
+        return Jest.Expect.toBe("#ed9f50", Jest.Expect.expect(Curry._2(Polished.Color.adjustHue, "#ed5051", 30.5)));
       }));
 
 Jest.test("complement", (function (param) {
-        return Jest.Expect.toBe("#50edec", Jest.Expect.expect(Polished.complement("#ed5051")));
+        return Jest.Expect.toBe("#50edec", Jest.Expect.expect(Curry._1(Polished.Color.complement, "#ed5051")));
       }));
 
 Jest.test("desaturate", (function (param) {
-        return Jest.Expect.toBe("#9f9f9f", Jest.Expect.expect(Polished.desaturate("#ed5051", 25.5)));
+        return Jest.Expect.toBe("#9f9f9f", Jest.Expect.expect(Curry._2(Polished.Color.desaturate, "#ed5051", 25.5)));
       }));
 
 Jest.test("getContrast", (function (param) {
-        return Jest.Expect.toBe(3.58, Jest.Expect.expect(Polished.getContrast("#ed5051", "#ffffff")));
+        return Jest.Expect.toBe(3.58, Jest.Expect.expect(Curry._2(Polished.Color.getContrast, "#ed5051", "#ffffff")));
       }));
 
 Jest.test("getLuminance", (function (param) {
-        return Jest.Expect.toBe(0.243, Jest.Expect.expect(Polished.getLuminance("#ed5051")));
+        return Jest.Expect.toBe(0.243, Jest.Expect.expect(Curry._1(Polished.Color.getLuminance, "#ed5051")));
       }));
 
 Jest.test("grayscale", (function (param) {
-        return Jest.Expect.toBe("#9f9f9f", Jest.Expect.expect(Polished.grayscale("#ed5051")));
+        return Jest.Expect.toBe("#9f9f9f", Jest.Expect.expect(Curry._1(Polished.Color.grayscale, "#ed5051")));
       }));
 
 Jest.test("hsla", (function (param) {
-        return Jest.Expect.toBe("rgba(96,159,106,0.5)", Jest.Expect.expect(Polished.hsla(130, 0.25, 0.5, 0.5)));
+        return Jest.Expect.toBe("rgba(96,159,106,0.5)", Jest.Expect.expect(Curry._4(Polished.Color.hsla, 130, 0.25, 0.5, 0.5)));
       }));
 
 Jest.test("invert", (function (param) {
-        return Jest.Expect.toBe("#12a6ae", Jest.Expect.expect(Polished.invert("#ed5951")));
+        return Jest.Expect.toBe("#12a6ae", Jest.Expect.expect(Curry._1(Polished.Color.invert, "#ed5951")));
       }));
 
 Jest.test("meetsContrastGuidelines", (function (param) {
@@ -67,11 +68,11 @@ Jest.test("meetsContrastGuidelines", (function (param) {
           AAA: true,
           AAALarge: true
         };
-        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Polished.meetsContrastGuidelines("#000000", "#ffffff")));
+        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Curry._2(Polished.Color.meetsContrastGuidelines, "#000000", "#ffffff")));
       }));
 
 Jest.test("transparentize", (function (param) {
-        return Jest.Expect.toBe("rgba(237,80,81,0.5)", Jest.Expect.expect(Polished.transparentize("#ed5051", 0.5)));
+        return Jest.Expect.toBe("rgba(237,80,81,0.5)", Jest.Expect.expect(Curry._2(Polished.Color.transparentize, "#ed5051", 0.5)));
       }));
 
 Jest.test("hslToColorString", (function (param) {
@@ -80,15 +81,15 @@ Jest.test("hslToColorString", (function (param) {
           lightness: 0.6215686274509804,
           saturation: 0.8134715025906737
         };
-        return Jest.Expect.toBe("#ed5051", Jest.Expect.expect(Polished.hslToColorString(hsl)));
+        return Jest.Expect.toBe("#ed5051", Jest.Expect.expect(Curry._1(Polished.Color.hslToColorString, hsl)));
       }));
 
 Jest.test("mix", (function (param) {
-        return Jest.Expect.toBe("#c58383", Jest.Expect.expect(Polished.mix("#ed5051", "#bc9090", 0.2)));
+        return Jest.Expect.toBe("#c58383", Jest.Expect.expect(Curry._3(Polished.Color.mix, "#ed5051", "#bc9090", 0.2)));
       }));
 
 Jest.test("opacify", (function (param) {
-        return Jest.Expect.toBe("rgba(237,80,81,0.5)", Jest.Expect.expect(Polished.opacify("#ed505100", 0.5)));
+        return Jest.Expect.toBe("rgba(237,80,81,0.5)", Jest.Expect.expect(Curry._2(Polished.Color.opacify, "#ed505100", 0.5)));
       }));
 
 Jest.test("parseToHsl", (function (param) {
@@ -97,7 +98,7 @@ Jest.test("parseToHsl", (function (param) {
           lightness: 0.6215686274509804,
           saturation: 0.8134715025906737
         };
-        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Polished.parseToHsl("#ed5051")));
+        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Curry._1(Polished.Color.parseToHsl, "#ed5051")));
       }));
 
 Jest.test("parseToRgb", (function (param) {
@@ -106,17 +107,17 @@ Jest.test("parseToRgb", (function (param) {
           green: 80,
           blue: 81
         };
-        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Polished.parseToRgb("#ed5051")));
+        return Jest.Expect.toEqual(expected, Jest.Expect.expect(Curry._1(Polished.Color.parseToRgb, "#ed5051")));
       }));
 
 Jest.test("readableColor", (function (param) {
-        Jest.Expect.toBe("#fff", Jest.Expect.expect(Polished.readableColor("#ed5051", undefined, undefined, false, undefined)));
-        Jest.Expect.toBe("#000", Jest.Expect.expect(Polished.readableColor("#000", undefined, undefined, undefined, undefined)));
-        Jest.Expect.toBe("#ff8", Jest.Expect.expect(Polished.readableColor("black", undefined, "#ff8", undefined, undefined)));
-        Jest.Expect.toBe("#001", Jest.Expect.expect(Polished.readableColor("white", "#001", undefined, undefined, undefined)));
-        Jest.Expect.toBe("#000", Jest.Expect.expect(Polished.readableColor("red", "#333", "#ddd", true, undefined)));
-        Jest.Expect.toBe("#333", Jest.Expect.expect(Polished.readableColor("yellow", "#333", "#ddd", true, undefined)));
-        return Jest.Expect.toBe("#ddd", Jest.Expect.expect(Polished.readableColor("blue", "#333", "#ddd", true, undefined)));
+        Jest.Expect.toBe("#fff", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "#ed5051", undefined, undefined, false, undefined)));
+        Jest.Expect.toBe("#000", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "#000", undefined, undefined, undefined, undefined)));
+        Jest.Expect.toBe("#ff8", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "black", undefined, "#ff8", undefined, undefined)));
+        Jest.Expect.toBe("#001", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "white", "#001", undefined, undefined, undefined)));
+        Jest.Expect.toBe("#000", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "red", "#333", "#ddd", true, undefined)));
+        Jest.Expect.toBe("#333", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "yellow", "#333", "#ddd", true, undefined)));
+        return Jest.Expect.toBe("#ddd", Jest.Expect.expect(Curry._5(Polished.Color.readableColor, "blue", "#333", "#ddd", true, undefined)));
       }));
 
 Jest.test("rgb", (function (param) {
@@ -125,7 +126,7 @@ Jest.test("rgb", (function (param) {
           green: 255,
           blue: 255
         };
-        return Jest.Expect.toBe("#fff", Jest.Expect.expect(Polished.rgb(rgb)));
+        return Jest.Expect.toBe("#fff", Jest.Expect.expect(Curry._1(Polished.Color.rgb, rgb)));
       }));
 
 Jest.test("rgba", (function (param) {
@@ -135,11 +136,11 @@ Jest.test("rgba", (function (param) {
           blue: 100,
           alpha: 0.7
         };
-        return Jest.Expect.toBe("rgba(255,205,100,0.7)", Jest.Expect.expect(Polished.rgba(rgba)));
+        return Jest.Expect.toBe("rgba(255,205,100,0.7)", Jest.Expect.expect(Curry._1(Polished.Color.rgba, rgba)));
       }));
 
 Jest.test("saturate", (function (param) {
-        return Jest.Expect.toBe("#ff3e3f", Jest.Expect.expect(Polished.saturate("#ed5051", 0.5)));
+        return Jest.Expect.toBe("#ff3e3f", Jest.Expect.expect(Curry._2(Polished.Color.saturate, "#ed5051", 0.5)));
       }));
 
 exports.keepGoing = keepGoing;
--- __tests__/PolishedTest.res
+++ __tests__/PolishedTest.res
@@ -1,99 +1,100 @@
 open Jest
 open Expect;
+open Polished;
 
 let keepGoing = _ => ()
 
 test("shade", () => {
   "#ed5051"
-  -> Polished.shade(~amount=0.25)
+  -> Color.shade(~amount=0.25)
   -> expect
   |> toBe("#b13c3c")
 })
 
 test("tint", () => {
   "#ed5051"
-  -> Polished.tint(~amount=0.25)
+  -> Color.tint(~amount=0.25)
   -> expect
   |> toBe("#f17b7c")
 })
 
 test("lighten", () => {
   "#ed5051"
-  -> Polished.lighten(~amount=0.25)
+  -> Color.lighten(~amount=0.25)
   -> expect
   |> toBe("#f9c4c4")
 })
 
 test("darken", () => {
   "#ed5051"
-  -> Polished.darken(~amount=0.25)
+  -> Color.darken(~amount=0.25)
   -> expect
   |> toBe("#ac1213")
 })
 
 test("hsl", () => {
-  Polished.hsl(~hue=30, ~saturation=0.5, ~lightness=0.3)
+  Color.hsl(~hue=30, ~saturation=0.5, ~lightness=0.3)
   -> expect
   |> toBe("#734d26")
 })
 
 test("adjustHue", () => {
   "#ed5051"
-  -> Polished.adjustHue(~degree=30.5)
+  -> Color.adjustHue(~degree=30.5)
   -> expect
   |> toBe("#ed9f50")
 })
 
 test("complement", () => {
   "#ed5051"
-  -> Polished.complement
+  -> Color.complement
   -> expect
   |> toBe("#50edec")
 })
 
 test("desaturate", () => {
   "#ed5051"
-  -> Polished.desaturate(~amount=25.5)
+  -> Color.desaturate(~amount=25.5)
   -> expect
   |> toBe("#9f9f9f")
 })
 
 test("getContrast", () => {
   "#ed5051"
-  -> Polished.getContrast("#ffffff")
+  -> Color.getContrast("#ffffff")
   -> expect
   |> toBe(3.58)
 })
 
 test("getLuminance", () => {
   "#ed5051"
-  -> Polished.getLuminance
+  -> Color.getLuminance
   -> expect
   |> toBe(0.243)
 })
 
 test("grayscale", () => {
   "#ed5051"
-  -> Polished.grayscale
+  -> Color.grayscale
   -> expect
   |> toBe("#9f9f9f")
 })
 
 test("hsla", () => {
-  -> Polished.hsla(~hue=130, ~saturation=0.25, ~lightness=0.5, ~alpha=0.5)
+  -> Color.hsla(~hue=130, ~saturation=0.25, ~lightness=0.5, ~alpha=0.5)
   -> expect
   |> toBe("rgba(96,159,106,0.5)")
 })
 
 test("invert", () => {
   "#ed5951"
-  -> Polished.invert
+  -> Color.invert
   -> expect
   |> toBe("#12a6ae")
 })
 
 test("meetsContrastGuidelines", () => {
-  let expected: Polished.contrastScores = {
+  let expected: Color.contrastScores = {
     "AA": true,
     "AALarge": true,
     "AAA": true,
@@ -101,98 +102,98 @@ test("meetsContrastGuidelines", () => {
   }
 
   ("#000000")
-  -> Polished.meetsContrastGuidelines("#ffffff")
+  -> Color.meetsContrastGuidelines("#ffffff")
   -> expect
   |> toEqual(expected)
 })
 
 test("transparentize", () => {
   "#ed5051"
-  -> Polished.transparentize(~amount=0.5)
+  -> Color.transparentize(~amount=0.5)
   -> expect
   |> toBe("rgba(237,80,81,0.5)")
 })
 
 test("hslToColorString", () => {
-  let hsl: Polished.hslColor = {
+  let hsl: Color.hslColor = {
     "hue": 359.61783439490443,
     "lightness": 0.6215686274509804,
     "saturation": 0.8134715025906737,
   }
 
   hsl
-  -> Polished.hslToColorString
+  -> Color.hslToColorString
   -> expect
   |> toBe("#ed5051")
 })
 
 test("mix", () => {
   "#ed5051"
-  -> Polished.mix("#bc9090", ~weight=0.2)
+  -> Color.mix("#bc9090", ~weight=0.2)
   -> expect
   |> toBe("#c58383")
 })
 
 test("opacify", () => {
   "#ed505100"
-  -> Polished.opacify(~amount=0.5)
+  -> Color.opacify(~amount=0.5)
   -> expect
   |> toBe("rgba(237,80,81,0.5)")
 })
 
 test("parseToHsl", () => {
-  let expected: Polished.hslColor = {
+  let expected: Color.hslColor = {
     "hue": 359.61783439490443,
     "lightness": 0.6215686274509804,
     "saturation": 0.8134715025906737,
   }
 
   "#ed5051"
-  -> Polished.parseToHsl
+  -> Color.parseToHsl
   -> expect
   |> toEqual(expected)
 })
 
 test("parseToRgb", () => {
-  let expected: Polished.rgbColor = {
+  let expected: Color.rgbColor = {
     "red": 237,
     "green": 80,
     "blue": 81
   }
 
   "#ed5051"
-  -> Polished.parseToRgb
+  -> Color.parseToRgb
   -> expect
   |> toEqual(expected)
 })
 
 test("readableColor", () => {
   "#ed5051"
-  -> Polished.readableColor(~strict=false, ())
+  -> Color.readableColor(~strict=false, ())
   -> expect
   |> toBe("#fff")
   |> keepGoing
 
   "#000"
-  -> Polished.readableColor(())
+  -> Color.readableColor(())
   -> expect
   |> toBe("#000")
   |> keepGoing
 
   "black"
-  -> Polished.readableColor(~darkReturnColor="#ff8", ())
+  -> Color.readableColor(~darkReturnColor="#ff8", ())
   -> expect
   |> toBe("#ff8")
   |> keepGoing
 
   "white"
-  -> Polished.readableColor(~lightReturnColor="#001", ())
+  -> Color.readableColor(~lightReturnColor="#001", ())
   -> expect
   |> toBe("#001")
   |> keepGoing
 
   "red"
-  -> Polished.readableColor(
+  -> Color.readableColor(
     ~lightReturnColor = "#333",
     ~darkReturnColor = "#ddd",
     ~strict = true,
@@ -203,7 +204,7 @@ test("readableColor", () => {
   |> keepGoing
 
   "yellow"
-  -> Polished.readableColor(
+  -> Color.readableColor(
     ~lightReturnColor = "#333",
     ~darkReturnColor = "#ddd",
     ~strict = true,
@@ -214,7 +215,7 @@ test("readableColor", () => {
   |> keepGoing
 
   "blue"
-  -> Polished.readableColor(
+  -> Color.readableColor(
     ~lightReturnColor = "#333",
     ~darkReturnColor = "#ddd",
     ~strict = true,
@@ -225,20 +226,20 @@ test("readableColor", () => {
 })
 
 test("rgb", () => {
-  let rgb: Polished.rgbColor = {
+  let rgb: Color.rgbColor = {
     "red": 255,
     "green": 255,
     "blue": 255
   }
 
   rgb
-  -> Polished.rgb
+  -> Color.rgb
   -> expect
   |> toBe("#fff")
 })
 
 test("rgba", () => {
-  let rgba: Polished.rgbaColor = {
+  let rgba: Color.rgbaColor = {
     "red": 255,
     "green": 205,
     "blue": 100,
@@ -246,14 +247,14 @@ test("rgba", () => {
   }
 
   rgba
-  -> Polished.rgba
+  -> Color.rgba
   -> expect
   |> toBe("rgba(255,205,100,0.7)")
 })
 
 test("saturate", () => {
   "#ed5051"
-  -> Polished.saturate(~amount=0.5)
+  -> Color.saturate(~amount=0.5)
   -> expect
   |> toBe("#ff3e3f")
 })
